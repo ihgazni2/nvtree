4ab4d6a271a825209638247265ba6c00a0665c7d
navegador5 <55095363+navegador5@users.noreply.github.com>
2020-03-22 20:32:28 +0800

    Update tree.draft.js

--- tree.draft.js
+++ tree.draft.js
@@ -1,27 +1,25 @@
 const ndfunc = require('./ndfunc.js')
 
+
+
 class Node {
-    constructor(id=0) {
-        this._id = id;
-        this._fstch = null,
-        this._lsib = undefined,
-        this._rsib = undefined,
-        this._parent = undefined,
-        this._tree = undefined    
+    constructor() {
+        this._fstch = null
+        this._lsib = undefined
+        this._rsib = undefined
+        this._parent = undefined
     }
     
 }
 
 
 class Root extends Node {
-    constructor(id=0) {
-        super(id);
-        this._id = id;
-        this._fstch = null,
-        this._lsib = null,
-        this._rsib = null,
-        this._parent = null,
-        this._tree = id 
+    constructor() {
+        super();
+        this._fstch = null
+        this._lsib = null
+        this._rsib = null
+        this._parent = null
     }
 }
 
